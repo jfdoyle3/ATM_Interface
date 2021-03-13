7fd0d773efd9a3ca49676c24930c8c6ced0934fa
jfdoyle3 <indellibleink@gmail.com>
2021-03-13 15:45:52 -0500

    Added projects objects

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Account {
+
+    // Name: Checking / Savings account
+    private String accountName;
+
+    private double balance;
+
+    // Account ID number
+    private String uuid;
+
+    private User holder;
+
+    private ArrayList<Transaction> transaction;
+
+
+
+}
